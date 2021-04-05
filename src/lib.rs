d30562ebc1614d7e1954e47e2a5d9caad1199199
Daniel <dmleet1@gmail.com>
2021-04-05 01:48:08 -0400

    Added rotation for blocks

--- src/lib.rs
+++ src/lib.rs
@@ -39,8 +39,8 @@ use self::BlockStyle::*;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Coord {
-    x: usize,
-    y: usize,
+    x: i32,
+    y: i32,
 }
 
 impl std::ops::Add<Coord> for Coord {
@@ -52,7 +52,7 @@ impl std::ops::Add<Coord> for Coord {
 }
 
 impl Coord {
-    pub fn new(x: usize, y: usize) -> Coord {
+    pub fn new(x: i32, y: i32) -> Coord {
         Coord { x, y }
     }
 }
@@ -69,54 +69,53 @@ impl Block {
     // unsafe for now, caused by the shit js_sys methods
     // next block style will come from the server
     pub fn next() -> Block {
-        let rnd =js_sys::Math::floor(
-            js_sys::Math::random() * 7.0) as usize;
-        let style = [ I, J, L, O, S, T, Z ][rnd];
+        let rnd = js_sys::Math::floor(js_sys::Math::random() * 7.0) as usize;
+        let style = [I, J, L, O, S, T, Z][rnd];
         Block {
             style,
             color: JsValue::from_str("coral"),
             coords: match style {
                 BlockStyle::I => [
-                    Coord::new(1, 0),
-                    Coord::new(1, 1),
-                    Coord::new(1, 2),
-                    Coord::new(1, 3),
+                    Coord::new(0, -2),
+                    Coord::new(0, -1),
+                    Coord::new(0, 0),
+                    Coord::new(0, 1),
                 ],
                 BlockStyle::J => [
-                    Coord::new(1, 0),
-                    Coord::new(1, 1),
-                    Coord::new(1, 2),
-                    Coord::new(0, 2),
+                    Coord::new(0, -2),
+                    Coord::new(0, -1),
+                    Coord::new(0, 0),
+                    Coord::new(-1, 0),
                 ],
                 BlockStyle::L => [
-                    Coord::new(1, 0),
-                    Coord::new(1, 1),
-                    Coord::new(1, 2),
-                    Coord::new(2, 2),
-                ],
-                BlockStyle::O => [
-                    Coord::new(1, 0),
-                    Coord::new(2, 0),
-                    Coord::new(2, 1),
-                    Coord::new(1, 1),
-                ],
-                BlockStyle::S => [
-                    Coord::new(0, 1),
-                    Coord::new(1, 1),
-                    Coord::new(1, 0),
-                    Coord::new(2, 0),
-                ],
-                BlockStyle::T => [
-                    Coord::new(0, 1),
-                    Coord::new(1, 1),
-                    Coord::new(2, 1),
-                    Coord::new(1, 2),
-                ],
-                BlockStyle::Z => [
+                    Coord::new(0, -2),
+                    Coord::new(0, -1),
                     Coord::new(0, 0),
                     Coord::new(1, 0),
-                    Coord::new(1, 1),
-                    Coord::new(2, 1),
+                ],
+                BlockStyle::O => [
+                    Coord::new(0, -1),
+                    Coord::new(1, -1),
+                    Coord::new(1, 0),
+                    Coord::new(0, 0),
+                ],
+                BlockStyle::S => [
+                    Coord::new(-1, 0),
+                    Coord::new(0, 0),
+                    Coord::new(0, -1),
+                    Coord::new(1, -1),
+                ],
+                BlockStyle::T => [
+                    Coord::new(-1, 0),
+                    Coord::new(0, 0),
+                    Coord::new(1, 0),
+                    Coord::new(0, -1),
+                ],
+                BlockStyle::Z => [
+                    Coord::new(-1, -1),
+                    Coord::new(0, -1),
+                    Coord::new(0, 0),
+                    Coord::new(1, 0),
                 ],
             },
         }
@@ -129,15 +128,15 @@ impl Block {
             let abs_coords = pos + coord.clone();
             context.set_fill_style(&self.color);
             context.fill_rect(
-                (abs_coords.x * cell_size_px) as f64,
-                (abs_coords.y * cell_size_px) as f64,
+                (abs_coords.x * cell_size_px as i32) as f64,
+                (abs_coords.y * cell_size_px as i32) as f64,
                 cell_size_px as f64,
                 cell_size_px as f64,
             );
             context.set_stroke_style(&JsValue::from_str("black"));
             context.stroke_rect(
-                (abs_coords.x * cell_size_px) as f64,
-                (abs_coords.y * cell_size_px) as f64,
+                (abs_coords.x * cell_size_px as i32) as f64,
+                (abs_coords.y * cell_size_px as i32) as f64,
                 cell_size_px as f64,
                 cell_size_px as f64,
             );
@@ -153,7 +152,6 @@ pub struct Board {
     cells: Vec<Vec<Cell>>,
     block: Block,
     pos: Coord,
-    rotation: usize,
 }
 
 impl Board {
@@ -164,19 +162,22 @@ impl Board {
             cell_size_px,
             cells: vec![vec![Cell::Empty; cols]; rows],
             block: Block::next(),
-            pos: Coord::new(4, 0),
-            rotation: 0,
+            pos: Coord::new((cols / 2) as i32, 0),
         }
     }
 }
 
 impl Board {
-    fn check_collision(&mut self, new_pos: Coord) -> bool {
-        self.block.coords.iter().any(|coord| {
-            let x = &coord.x + new_pos.x;
-            let y = &coord.y + new_pos.y;
-            // TODO - why does this not throw index oob? could this ever cause fuckiness?
-            y >= self.rows || self.cells[y][x] == Cell::Filled
+    fn check_collision(&mut self, new_pos: Coord, new_coords: BlockCoords) -> bool {
+        new_coords.iter().any(|coord| {
+            let abs_x = &coord.x + new_pos.x;
+            let abs_y = &coord.y + new_pos.y;
+            abs_x < 0
+                || abs_x >= self.cols as i32
+                || abs_y >= self.rows as i32
+                || (abs_x >= 0
+                    && abs_y >= 0
+                    && self.cells[abs_y as usize][abs_x as usize] == Cell::Filled)
         })
     }
 }
@@ -184,11 +185,10 @@ impl Board {
 impl Board {
     fn fill_block(&mut self) {
         for coord in self.block.coords.iter() {
-            self.cells[coord.y + self.pos.y][coord.x + self.pos.x] = Cell::Filled;
+            let x = (coord.x + self.pos.x) as usize;
+            let y = (coord.y + self.pos.y) as usize;
+            self.cells[y][x] = Cell::Filled;
         }
-
-        self.pos = Coord::new(4, 0);
-        self.block = Block::next();
     }
 }
 
@@ -249,21 +249,25 @@ impl Game {
                         (col * self.board.cell_size_px) as f64,
                         (row * self.board.cell_size_px) as f64,
                         self.board.cell_size_px as f64,
-                        self.board.cell_size_px as f64);
+                        self.board.cell_size_px as f64,
+                    );
                 }
             }
         }
 
         // Active Block
-        self.board.block.draw(&self.context, self.board.pos, self.board.cell_size_px);
+        self.board
+            .block
+            .draw(&self.context, self.board.pos, self.board.cell_size_px);
 
         // Border
         self.context.set_stroke_style(border);
         self.context.stroke_rect(
-            0.0, 
-            0.0, 
-            (self.board.cols * self.board.cell_size_px) as f64, 
-            (self.board.rows * self.board.cell_size_px) as f64);
+            0.0,
+            0.0,
+            (self.board.cols * self.board.cell_size_px) as f64,
+            (self.board.rows * self.board.cell_size_px) as f64,
+        );
     }
 }
 
@@ -271,14 +275,66 @@ impl Game {
 impl Game {
     pub fn tick(&mut self) {
         let new_pos = Coord::new(self.board.pos.x, self.board.pos.y + 1);
-        //let msg = format!("{:?}", new_pos);
-        //alert(&msg);
-        if self.board.check_collision(new_pos.clone()) {
+        if self
+            .board
+            .check_collision(new_pos.clone(), self.board.block.coords)
+        {
             self.board.fill_block();
+            self.board.pos = Coord::new((self.board.cols / 2) as i32, 0);
+            self.board.block = Block::next();
         } else {
             self.board.pos = new_pos;
         }
         self.draw();
+    }
+}
+
+#[wasm_bindgen]
+impl Game {
+    pub fn move_left(&mut self) {
+        let new_pos = Coord::new(self.board.pos.x - 1, self.board.pos.y);
+        if !self
+            .board
+            .check_collision(new_pos.clone(), self.board.block.coords)
+        {
+            self.board.pos = new_pos;
+            self.draw();
+        }
+    }
+
+    pub fn move_right(&mut self) {
+        let new_pos = Coord::new(self.board.pos.x + 1, self.board.pos.y);
+        if !self
+            .board
+            .check_collision(new_pos.clone(), self.board.block.coords)
+        {
+            self.board.pos = new_pos;
+            self.draw();
+        }
+    }
+
+    pub fn move_down(&mut self) {
+        let new_pos = Coord::new(self.board.pos.x, self.board.pos.y + 1);
+        if !self
+            .board
+            .check_collision(new_pos.clone(), self.board.block.coords)
+        {
+            self.board.pos = new_pos;
+            self.draw();
+        }
+    }
+
+    pub fn rotate(&mut self) {
+        let mut new_coords = self.board.block.coords;
+        for coord in new_coords.iter_mut() {
+            let new_y = -coord.x;
+            coord.x = coord.y;
+            coord.y = new_y;
+        }
+        if !self.board.check_collision(self.board.pos, new_coords) {
+            self.board.block.coords = new_coords;
+            self.draw();
+        }
     }
 }
 
